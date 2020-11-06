ab3c558c8c75a979df40eb9f4a5a0cd75bedce7a
liguanghui02 <liguanghui02@meituan.com>
2020-11-07 00:21:30 +0800

    fix

--- src/test/java/TestSpring.java
+++ src/test/java/TestSpring.java
@@ -1,18 +1,16 @@
 import com.xiya.service.PersonManager;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
-* @file Test.java
-* @CopyRight (C) http://blog.csdn.net/x_iya
-* @Description
-* @author N3verL4nd
-* @email lgh1992314@qq.com
-* @date 2017/6/17
-*/
-
-public class Test {
-    @org.junit.Test
+ * @author N3verL4nd
+ * @date 2020/11/7
+ */
+public class TestSpring {
+    @Test
     public void test() {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         PersonManager peopleManager = context.getBean(PersonManager.class);
